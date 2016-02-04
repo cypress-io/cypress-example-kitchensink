c01ffaa29bf23fa4bdcdfd6526bbfad6256e5191
Jennifer Mann <shehane.jennifer@gmail.com>
2016-02-04 14:45:18 -0500

    Started to write querying example tests

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -39,9 +39,12 @@ describe('Kitchen Sink [000]', function(){
     // state buildup we may have
 
     // http://on.cypress.io/visit
-    cy.visit('http://localhost:8080/')
 
-    // TODO: Replace the url string with your local URL to visit your app.
+    // cy.visit('http://cypress-io.github.io/examples-kitchen-sink/')
+    cy.visit('http://localhost:8080')
+
+    // TODO: Replace the url string with your locally hosted URL
+    //       to visit your app (like http://localhost:8080).
 
   })
 
@@ -52,6 +55,9 @@ describe('Kitchen Sink [000]', function(){
     // Here we've made our first assertion using a '.should()' command.
     // An assertion is comprised of a chainer, subject, and optional value
     // Chainers are available from Chai, Chai-jQuery, and Chai-Sinon
+    //
+    // http://on.cypress.io/should
+    // http://on.cypress.io/and
 
     // http://on.cypress.io/title
     cy.title().should('include', 'Kitchen Sink')
@@ -59,6 +65,42 @@ describe('Kitchen Sink [000]', function(){
     // subject        chainer      value
 
     // TODO: Replace "Kitchen Sink" with the title of your app.
+
+  })
+
+  context('Querying [002]', function(){
+
+    // Let's query for some DOM elements and make assertions
+    // The most commonly used query is "cy.get()", you can
+    // think of this like the "$"" in jQueury
+
+    it('use cy.get() to query DOM elements [007]', function(){
+
+      // http://on.cypress.io/get
+      cy.get('p:nth-of-type(1)').should('contain', 'To query for this DOM element')
+      //              ↲
+      // we can use CSS selectors just like jQuery
+
+      // we can get elements by their ID
+      cy.get('#id-example').should('contain', 'We can get elements')
+
+      // we can get elements by their class
+      cy.get('.class-example').should('contain', 'We can get elements')
+
+
+    })
+
+    it('use cy.contains() to query DOM elements with matching content [008]', function(){
+
+      // http://on.cypress.io/get
+      cy.contains('We can find elements by their content').should('contain', 'We can find elements by their content')
+      //              ↲
+      // we can use CSS selectors just like jQuery
+
+      // we can get elements by their ID
+      cy.contains('Shazam').should('contain', 'Shazam Three')
+
+    })
 
   })
 
