50a8c29943544c491d54758157c55414f8cbe08b
Jennifer Mann <shehane.jennifer@gmail.com>
2016-02-08 16:38:48 -0500

    Added Window specific commands.

--- tests/example_spec.js
+++ tests/example_spec.js
@@ -260,7 +260,7 @@ describe('Kitchen Sink [000]', function(){
     })
 
 
-    it.only('cy.clear() - clears the value of an input or textarea element [00a]', function(){
+    it('cy.clear() - clears the value of an input or textarea element [00a]', function(){
 
       // http://on.cypress.io/clear
       cy
@@ -356,12 +356,12 @@ describe('Kitchen Sink [000]', function(){
       // cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').check().should('be.checked')
       // cy.get('.action-radios [type="radio"]').not('[disabled]').check().should('be.checked')
 
-      // // **** Check Value ****
-      // //
-      // // cy.check() accepts a value argument
-      // // that checks only checkboxes or radios
-      // // with matching values
-      // //
+      // **** Check Value ****
+      //
+      // cy.check() accepts a value argument
+      // that checks only checkboxes or radios
+      // with matching values
+      //
       // cy.get('.action-radios [type="radio"]').check('radio1').should('be.checked')
 
       // **** Check Options ****
@@ -429,6 +429,116 @@ describe('Kitchen Sink [000]', function(){
 
     })
 
+
+  })
+
+  context('Window', function(){
+
+    // **** Window ****
+    //
+    // Cypress has commands to help you get
+    // access to window, document, and title
+
+    it('cy.window() - get the global window object', function(){
+
+      // http://on.cypress.io/window
+      cy.window().should('have.property', 'top')
+
+    })
+
+    it('cy.document() - get the document object', function(){
+
+      // http://on.cypress.io/document
+      cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+
+    })
+
+    it('cy.title() - get the title', function(){
+
+      // http://on.cypress.io/title
+      cy.title().should('include', 'Kitchen Sink')
+
+    })
+  })
+
+
+  context('Viewport', function(){
+
+    // **** Viewport ****
+    //
+    // Let's make some assertions based on
+    // the size of our screen. This command
+    // is great for checking responsive logic
+
+    it('cy.viewport() - set the viewport size and dimension', function(){
+
+      cy.get('#navbar').should('be.visible')
+
+      // http://on.cypress.io/viewport
+      cy.viewport(320, 480)
+
+      // the navbar should have collapse since our
+      // screen is smaller
+      cy.get('#navbar').should('not.be.visible')
+      cy.get('.navbar-toggle').should('be.visible').click()
+      cy.get('.nav').find('a').should('be.visible')
+
+      // lets see what our app looks like on a super large screen
+      cy.viewport(2999, 2999)
+      cy.wait(200, {log: false})
+
+      // **** Viewport Presets ****
+      //
+      // cy.viewport() accepts a set of preset sizes
+      // to easily set the screen to a device's width and height
+      // We added a wait between each viewport change so you can see
+      // the change otherwise it's a little too fast to see :)
+      //
+      cy.viewport("macbook-15")
+      cy.wait(200, {log: false})
+
+      cy.viewport("macbook-13")
+      cy.wait(200, {log: false})
+
+      cy.viewport("macbook-11")
+      cy.wait(200, {log: false})
+
+      cy.viewport("ipad-2")
+      cy.wait(200, {log: false})
+
+      cy.viewport("ipad-mini")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-6+")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-6")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-5")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-4")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-3")
+      cy.wait(200, {log: false})
+
+      // **** Viewport Orientation ****
+      //
+      // cy.viewport() accepts an orientation for all presets
+      // the default orientation is 'portrait'
+      //
+      cy.viewport("ipad-2", "portrait")
+      cy.wait(200, {log: false})
+
+      cy.viewport("iphone-4", "landscape")
+      cy.wait(200, {log: false})
+
+      // The viewport will be reset back to the default dimensions
+      // in between tests (the  default is set in cypress.json)
+
+    })
 
   })
 
