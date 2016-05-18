854817f79af4eb5f7af55808346bf9516d560750
Brian Mann <brian@backbonerails.com>
2016-05-18 11:51:18 -0400

    clear 3rd party cookies after visit

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -1182,6 +1182,10 @@ describe('Kitchen Sink', function(){
       Cypress.Cookies.debug(true)
 
       cy.visit('http://localhost:8080/commands/cookies')
+
+      // clear cookies again after visiting to remove
+      // any 3rd party cookies picked up such as cloudflare
+      .clearCookies()
     })
 
     it('cy.getCookie() - get a browser cookie', function(){
