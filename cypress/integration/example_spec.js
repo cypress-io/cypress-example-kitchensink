d4239e4116ac531ae835b60359008a4e3fffe832
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 14:54:40 -0500

    run single test while debugging codeship

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -12,7 +12,7 @@
 // https://on.cypress.io/introduction-to-cypress
 
 describe('Kitchen Sink', function () {
-  it('.should() - assert that <title> is correct', function () {
+  it.only('.should() - assert that <title> is correct', function () {
     // https://on.cypress.io/visit
     cy.visit('http://localhost:8080')
 
