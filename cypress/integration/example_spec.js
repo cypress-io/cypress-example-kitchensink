adf73e7a8a510bdb485293dedcb4f9e59970d748
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 16:40:56 -0500

    enable all unit tests in this project

--- cypress/integration/example_spec.js
+++ cypress/integration/example_spec.js
@@ -741,7 +741,7 @@ describe('Kitchen Sink', function () {
       })
     })
 
-    it.only('cy.exec() - execute a system command', function () {
+    it('cy.exec() - execute a system command', function () {
       // cy.exec allows you to execute a system command.
       // so you can take actions necessary for your test,
       // but outside the scope of Cypress.
@@ -753,18 +753,18 @@ describe('Kitchen Sink', function () {
       // we can use Cypress.platform string to
       // select appropriate command
       // https://on.cypress/io/platform
-      // cy.log(`Platform ${Cypress.platform} architecture ${Cypress.arch}`)
+      cy.log(`Platform ${Cypress.platform} architecture ${Cypress.arch}`)
 
-      // if (Cypress.platform === 'win32') {
-      //   cy.exec('print cypress.json')
-      //     .its('stderr').should('be.empty')
-      // } else {
-      //   cy.exec('cat cypress.json')
-      //     .its('stderr').should('be.empty')
+      if (Cypress.platform === 'win32') {
+        cy.exec('print cypress.json')
+          .its('stderr').should('be.empty')
+      } else {
+        cy.exec('cat cypress.json')
+          .its('stderr').should('be.empty')
 
-      //   cy.exec('pwd')
-      //     .its('code').should('eq', 0)
-      // }
+        cy.exec('pwd')
+          .its('code').should('eq', 0)
+      }
     })
 
     it('cy.focused() - get the DOM element that has focus', function () {
