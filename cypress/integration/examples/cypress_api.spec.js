432d44bb06ad952b26dbdc96481883c63f87b87d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-06-08 13:08:39 -0400

    Type check specs 71 (#82)
    * typechecking some spec files for #71
    
    * type check all files with a few exceptions
    
    * explicit package lock
    
    * working on types in utilities.spec.js
    
    * enable all cy.visit
    
    * update contentWindow test to avoid crashing

--- cypress/integration/examples/cypress_api.spec.js
+++ cypress/integration/examples/cypress_api.spec.js
@@ -18,6 +18,7 @@ context('Cypress.Commands', () => {
       method = method || 'log'
 
       // log the subject to the console
+      // @ts-ignore TS7017
       // eslint-disable-next-line no-console
       console[method]('The subject is', subject)
 
@@ -27,6 +28,7 @@ context('Cypress.Commands', () => {
       return subject
     })
 
+    // @ts-ignore TS2339
     // eslint-disable-next-line no-unused-vars
     cy.get('button').console('info').then(($button) => {
       // subject is still $button
@@ -89,6 +91,7 @@ context('Cypress.Server', () => {
       // eslint-disable-next-line no-unused-vars
       whitelist (xhr) {
         // handle custom logic for whitelisting
+        expect(xhr).to.be.an('object')
       },
     })
   })
