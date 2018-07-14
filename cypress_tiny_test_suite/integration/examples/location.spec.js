20b60cdcc97825975c0ecc13dcef3d73a789080d
ArturT <arturtrzop@gmail.com>
2018-07-14 12:20:14 +0200

    Create copy of cypress test suite and name it tiny test suite

---
+++ cypress_tiny_test_suite/integration/examples/location.spec.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+context('Location', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/commands/location')
+  })
+
+  it('cy.hash() - get the current URL hash', () => {
+    // https://on.cypress.io/hash
+    cy.hash().should('be.empty')
+  })
+
+  it('cy.location() - get window.location', () => {
+    // https://on.cypress.io/location
+    cy.location().should((location) => {
+      expect(location.hash).to.be.empty
+      expect(location.href).to.eq('http://localhost:8080/commands/location')
+      expect(location.host).to.eq('localhost:8080')
+      expect(location.hostname).to.eq('localhost')
+      expect(location.origin).to.eq('http://localhost:8080')
+      expect(location.pathname).to.eq('/commands/location')
+      expect(location.port).to.eq('8080')
+      expect(location.protocol).to.eq('http:')
+      expect(location.search).to.be.empty
+    })
+  })
+
+  it('cy.url() - get the current URL', () => {
+    // https://on.cypress.io/url
+    cy.url().should('eq', 'http://localhost:8080/commands/location')
+  })
+})
