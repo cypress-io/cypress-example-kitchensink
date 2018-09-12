3ffb15c995d90a259c6909f0bb0fc23f403c1813
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-09-11 21:13:52 -0400

    move comment

--- cypress/integration/examples/network_requests.spec.js
+++ cypress/integration/examples/network_requests.spec.js
@@ -57,13 +57,13 @@ context('Network Requests', () => {
   })
 
   it('cy.request() - verify response using BDD syntax', () => {
-    // https://on.cypress.io/assertions
     cy.request('https://jsonplaceholder.typicode.com/comments')
-      .then((response) => {
-        expect(response).property('status').to.equal(200)
-        expect(response).property('body').to.have.length(500)
-        expect(response).to.include.keys('headers', 'duration')
-      })
+    .then((response) => {
+      // https://on.cypress.io/assertions
+      expect(response).property('status').to.equal(200)
+      expect(response).property('body').to.have.length(500)
+      expect(response).to.include.keys('headers', 'duration')
+    })
   })
 
   it('cy.route() - route responses to matching requests', () => {
