ed9a9613668e004c3fbfe8dadd400867b277069a
Chris Breiding <chrisbreiding@gmail.com>
2018-09-19 14:30:35 -0400

    rename and update content of example background file for cypress 4

--- cypress/background/index.js
+++ cypress/background/index.js
@@ -1,17 +1,17 @@
 // ***********************************************************
-// This example plugins/index.js can be used to load plugins
+// This example background/index.js can be used to load plugins
+// in the background process
 //
 // You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
+// the background file with the 'backgroundFile' configuration option.
 //
 // You can read more here:
-// https://on.cypress.io/plugins-guide
+// https://on.cypress.io/background-process
 // ***********************************************************
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
