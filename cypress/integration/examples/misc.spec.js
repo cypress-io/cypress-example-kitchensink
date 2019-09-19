1c5483e3a082df88db2b9dc9367cb989352e855a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-09-19 14:31:42 -0400

    try to not do echo on windows

--- cypress/integration/examples/misc.spec.js
+++ cypress/integration/examples/misc.spec.js
@@ -25,8 +25,6 @@ context('Misc', () => {
     // execute a system command.
     // so you can take actions necessary for
     // your test outside the scope of Cypress.
-    cy.exec('echo Jane Lane')
-      .its('stdout').should('contain', 'Jane Lane')
 
     // we can use Cypress.platform string to
     // select appropriate command
@@ -42,6 +40,9 @@ context('Misc', () => {
 
       cy.exec('pwd')
         .its('code').should('eq', 0)
+
+      cy.exec('echo Jane Lane')
+        .its('stdout').should('contain', 'Jane Lane')
     }
   })
 
