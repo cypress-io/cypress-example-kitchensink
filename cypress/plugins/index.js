af09daf4356d456e2286e10b14b892887a537982
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-11-12 13:19:39 -0500

    find and return Microsoft Edge browser for testing

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,5 @@
+const execa = require('execa')
+
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -11,8 +13,37 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+const findEdgeBrowser = () => {
+  const edgePath =
+    '/Applications/Microsoft Edge Canary.app/Contents/MacOS/Microsoft Edge Canary'
+
+  return execa(edgePath, ['--version']).then(result => {
+    // something like "Microsoft Edge 80.0.328.2 Canary"
+    console.log('browser version', result)
+    const [, version] = /Microsoft Edge (\d+\.\d+\.\d+\.\d+)/.exec(
+      result.stdout
+    )
+    const majorVersion = parseInt(version.split('.')[0])
+
+    return {
+      name: 'Edge Beta',
+      family: 'chrome',
+      displayName: 'Edge Beta',
+      version,
+      path: edgePath,
+      majorVersion
+    }
+  })
+}
+
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  return findEdgeBrowser().then(edgeBrowser => {
+  return {
+    browsers: config.browsers.concat(edgeBrowser)
+  }
+})
+
 }
