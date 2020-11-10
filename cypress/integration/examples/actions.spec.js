cb6453073570e50498d38929935071a458a7f50e
Artur Trzop <arturtrzop@gmail.com>
2020-11-10 12:32:27 +0100

    Do test retries in Cypress
    https://docs.cypress.io/guides/guides/test-retries.html#How-It-Works

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -10,7 +10,7 @@ context('Actions', () => {
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
     cy.get('.action-email')
-      .type('fake@email.com').should('have.value', 'fake@email.com')
+      .type('fake@email.com').should('have.value', 'xfake@email.com')
 
       // .type() with special character sequences
       .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
