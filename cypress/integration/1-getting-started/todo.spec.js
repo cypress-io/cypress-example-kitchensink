70a04eff998b4ab5ed746271092d7a619fbade0e
RastoH <rastislav.hudak@solargis.com>
2021-07-02 13:33:26 +0200

    test ci-2

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -24,7 +24,7 @@ describe('example to-do app', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
-    cy.get('.todo-list li').should('have.length.at.least', 2)
+    cy.get('.todo-list li').should('have.length', 2)
 
     // We can go even further and check that the default todos each contain
     // the correct text. We use the `first` and `last` functions
