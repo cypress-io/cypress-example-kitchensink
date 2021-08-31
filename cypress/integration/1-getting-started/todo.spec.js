21230f93a81dbcf86885c094271286181f0db2ae
jbdeb <jean-baptiste.debize@epitech.eu>
2021-08-31 16:05:27 +0200

    [ee]

--- cypress/integration/1-getting-started/todo.spec.js
+++ cypress/integration/1-getting-started/todo.spec.js
@@ -20,7 +20,7 @@ describe('example to-do app', () => {
     cy.visit('http://localhost:8080/todo')
   })
 
-  it.only('displays two todo items by default', () => {
+  it('displays two todo items by default', () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are two matched items,
     // which are the two default items.
