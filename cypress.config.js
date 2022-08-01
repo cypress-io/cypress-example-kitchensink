4a17a5354d5f56ca8a0b2126c997b4e75845e92b
Rachel <Raquel.rudermano@gmail.com>
2022-08-01 15:25:33 -0700

    record

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'projectId': '4b7344',
+  projectId: "rixuvs",
   e2e: {}
 }
--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -9,7 +9,7 @@ context('Actions', () => {
 
   it('.type() - type into a DOM element', () => {
     // https://on.cypress.io/type
-    cy.get('.action-email')
+    cy.get('.trigger-failed-test')
       .type('fake@email.com').should('have.value', 'fake@email.com')
 
       // .type() with special character sequences
