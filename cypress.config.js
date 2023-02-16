2f627891cc33fcc8feaea1d43f6ad20ae0570155
Tyler Biethman <tbiethman@gmail.com>
2023-02-16 13:50:56 -0600

    Baseline commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 // Branch 2 Commit 2
+
 module.exports = {
-  projectId: 'tri6ny',
+  projectId: 'zz3qkj', // development 29a47616-1201-4a50-b709-32e8e8269eb7
+  // projectId: 'tri6ny', // staging 045a7542-7947-4024-853e-2857c77d7565
+  experimentalWebKitSupport: true,
   e2e: {
     setupNodeEvents: (on, config) => {
       on('before:spec', (spec) => {
--- cypress/e2e/spec-0.cy.js
+++ cypress/e2e/spec-0.cy.js
@@ -1,3 +1,20 @@
-it('works', () => {
+it('works 1', { retries: 3 }, () => {
+  const attempt = cy.state('runnable')._currentRetry
+
+  expect(attempt).to.eq(3)
+})
+
+it('works 2', () => {
+  cy.wait(4000)
+  expect(true).to.eq(true)
+})
+
+it('works 3', () => {
+  cy.wait(5000)
+  expect(true).to.eq(true)
+})
+
+it('works 4', () => {
+  cy.wait(6000)
   expect(true).to.eq(true)
 })
