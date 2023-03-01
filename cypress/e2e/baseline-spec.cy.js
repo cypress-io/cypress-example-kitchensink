318f3eb46cc178ace5532cfce7d26c2e088fe071
Tyler Biethman <tbiethman@gmail.com>
2023-03-01 09:22:19 -0600

    Fixing tests in basline

--- cypress/e2e/baseline-spec.cy.js
+++ cypress/e2e/baseline-spec.cy.js
@@ -6,18 +6,19 @@ describe('baseline functionality', () => {
 
   it('has a failing test', () => {
     cy.wait(500)
-    expect(true).to.eq(false)
+    expect(true).to.eq(true)
   })
 
-  // TODO: skipping for a reason
-  it.skip('has a skipped test', () => {
+  it('has a skipped test', () => {
     cy.wait(500)
     expect(true).to.eq(true)
   })
 
   it('has a flaky test', { retries: 3 }, () => {
-    const attempt = cy.state('runnable')._currentRetry
+    // const attempt = cy.state('runnable')._currentRetry
 
-    expect(attempt).to.eq(3)
+    // expect(attempt).to.eq(3)
+
+    expect(true).to.eq(true)
   })
 })
