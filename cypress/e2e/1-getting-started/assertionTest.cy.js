1ff3d3c94eb0a27396e50d44c859acdf00d2cd98
David G <david@WOVN.IO>
2023-04-10 23:54:06 +0900

    Assertions

---
+++ cypress/e2e/1-getting-started/assertionTest.cy.js
@@ -0,0 +1,45 @@
+// context('Shows an active class for the current page',()=>{
+//     it('The active Class',() =>{
+//         cy.visit('/commands/actions')
+//         cy.get('.dropdown-menu').find('li').eq(2).should('have.class','active')
+//     })
+
+//     it('Should not have an active class on inactive pages',()=>{
+//         cy.visit('/commands/actions');
+//         cy.get('.dropdown-menu').find('li').first().should('not.have.class','active').find('a').should('have.attr','href','/commands/querying')
+//     })
+// })
+
+context('Checking the pages exists',()=>{
+    it('The query page',()=>{
+        cy.visit('/commands/querying')
+        cy.get('.dropdown-menu').find('li').eq(0).should('have.class','active')
+    })
+
+    it('The traversal page',()=>{
+        cy.visit('/commands/traversal')
+        cy.get('.dropdown-menu').find('li').eq(1).should('have.class','active')
+    })
+
+    it('The actions page',()=>{
+        cy.visit('/commands/actions')
+        cy.get('.dropdown-menu').find('li').eq(2).should('have.class','active')
+    })
+
+    it('The window page',()=>{
+        cy.visit('/commands/window')
+        cy.get('.dropdown-menu').find('li').eq(3).should('have.class','active')
+    })
+})
+
+context('Eaxmple of negative assertion',()=>{
+    it('To check the dropdown menu with the first item with the "active" class inactive',()=>{
+        cy.visit('/commands/waiting')
+        cy.get('.dropdown-menu')
+            .find('li')
+            .first()
+            .should('not.have.class','active')
+            .find('a')
+            .should('have.attr','href','/commands/querying')
+    })
+})
