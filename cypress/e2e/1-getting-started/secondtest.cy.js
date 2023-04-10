26962de5515fceed9338393a28bf0bcd9c17bf35
David G <david@WOVN.IO>
2023-04-10 22:48:45 +0900

    Main second test

--- cypress/e2e/1-getting-started/secondtest.cy.js
+++ cypress/e2e/1-getting-started/secondtest.cy.js
@@ -3,25 +3,25 @@
 const navBarText = Cypress.env('navBarText');
 
 context('Second Test',()=>{
-    it('types into an email field',() =>{
-        cy.visit('/commands/actions')
-        cy.findByPlaceholderText('Email').type('test@email.com',{delay:200})//Delaying the typing speed
-        cy.should('have.value','test@email.com')
+  it('types into an email field',() =>{
+    cy.visit('/commands/actions')
+    cy.findByPlaceholderText('Email').type('test@email.com',{delay:200})//Delaying the typing speed
+    cy.should('have.value','test@email.com')
           .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
           .type('{backspace}{selectall}')
-        cy.get('.action-disabled')
+    cy.get('.action-disabled')
           .type('disabled error checking', { force: true })
           .should('have.value', 'disabled error checking') 
-        cy.log('Cypress log used') 
-        cy.wait(2000).then(()=>{
-            // console.log('test is finished')
-            fetch('https://api.spacexdata.com/v3/missions')
+    cy.log('Cypress log used') 
+    cy.wait(2000).then(()=>{
+      // console.log('test is finished')
+      fetch('https://api.spacexdata.com/v3/missions')
             .then((res)=>res.json())
             .then((data) => {
-                console.log(data)
+              console.log(data)
             })
-        });
+    });
 
-    })
+  })
 })
 
