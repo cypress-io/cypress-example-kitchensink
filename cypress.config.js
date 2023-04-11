d063d5daefdaa4f3914b5232bd34a34927d182f8
David G <david@WOVN.IO>
2023-04-12 05:50:41 +0900

    Checking workflow

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
+    // baseUrl:'https://example.cypress.io/'//For Lambda tests
   },
   env: {
     "navBarText":"cypress.io",
