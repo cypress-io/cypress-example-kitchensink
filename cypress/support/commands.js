cd0bed5356bc79e6b0b460d0731b552161830e66
David G <david@WOVN.IO>
2023-04-11 21:58:42 +0900

    Adding custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,3 +24,28 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import '@testing-library/cypress/add-commands'
+
+Cypress.Commands.add('setLocalStorage',(key,value)=>{
+    cy.window().then((window)=>{
+        window.localStorage.setItem(key,value)
+    })
+})
+
+Cypress.Commands.add('getLocalStorage',(key)=>{
+    cy.window().then((window)=>{
+        return window.localStorage.getItem(key)
+    })
+})
+
+Cypress.Commands.overwrite('type',(originalFn,element,text,options)=>{
+    if(options && options.sensitive){
+        options.log = false;
+        Cypress.log({
+            $el:element,
+            name:'type',
+            message:'*'.repeat(text.length)
+        })
+    }
+    return originalFn(element,text,options)
+})
+
