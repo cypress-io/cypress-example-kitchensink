1cb6bed7d3b43b7d9311a1514c39f9a193717ebc
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-04-20 15:27:15 +0200

    feat: update example tests for compatibility with latest recommended mocha lint (#633)
    * fix: ignore mocha/no-mocha-arrows linting rule
    
    * fix: merge sibling beforeeach in files example
    
    * fix: add top level context to cypress_api example
    
    * chore(deps): update eslint-plugin-mocha to 10.1.0

--- cypress/e2e/2-advanced-examples/files.cy.js
+++ cypress/e2e/2-advanced-examples/files.cy.js
@@ -7,9 +7,7 @@ const requiredExample = require('../../fixtures/example')
 context('Files', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/commands/files')
-  })
 
-  beforeEach(() => {
     // load example.json fixture file and store
     // in the test context object
     cy.fixture('example.json').as('example')
