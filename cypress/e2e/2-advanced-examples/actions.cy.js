55da5b9cdc6645d0d3a3d3c5f856f7a04e2455b1
ralucastr <129978480+ralucastr@users.noreply.github.com>
2023-05-04 14:09:58 +0300

    added tests for simple form demo

--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -35,7 +35,7 @@ context('Actions', () => {
 
   it('.focus() - focus on a DOM element', () => {
     // https://on.cypress.io/focus
-    cy.get('.action-focus').focus()
+    cy.get('#password1').focus()
       .should('have.class', 'focus')
       .prev().should('have.attr', 'style', 'color: orange;')
   })
