dae7296fb0caf42f61a088db30e425a3a84f05c9
nitayH <nitay@redefine.dev>
2023-05-20 14:03:05 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,11 @@ echo "--- NPM version"
 npm --version
 
 echo "--- Cypress version"
-"$(npm bin)/cypress" version
+npx cypress --version
 
 echo "--- Env vars:"
+chmod 777 scripts/read_envs.sh
 scripts/read_envs.sh
 
 echo "+++ Run Cypress tests"
-npm run e2e
+npx run test
