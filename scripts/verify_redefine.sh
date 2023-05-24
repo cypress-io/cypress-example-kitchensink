abc0617540db1047597350228aee3817408fbfa4
nitayH <nitay@redefine.dev>
2023-05-24 11:01:39 +0300

    pip3

--- scripts/verify_redefine.sh
+++ scripts/verify_redefine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip install -U redefine --index-url https://redefine.dev/pip/ 
+pip3 install -U redefine --index-url https://redefine.dev/pip/ 
 redefine config set stable_branch=master
 redefine verify --cypress
