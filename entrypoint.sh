b66870b1cc61ab37c8f8c844b095eb5d9bad52e9
liricooli <liricooli@gmail.com>
2023-06-01 16:44:13 +0300

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,18 +16,21 @@ scripts/read_envs.sh
 echo "--- Installing npm packages"
 npm install --save-dev
 npm install
+npm install cypress --save-dev
 
 echo "--- Verifying Redefine"
 # chmod 777 scripts/verify_redefine.sh
 # scripts/verify_redefine.sh
 # export REDEFINE_AUTH=e88afb65-65ef-4798-bc9d-db4c5342f7f3::163bf879-4060-4636-9fb6-49161e68095a
 pip3 install -U redefine --index-url https://redefine.dev/pip/
-# pip3 install ${BUILDKITE_BUILD_PATH}/redefine-0.14.15.post2-py3-none-manylinux1_x86_64.whl
+# pip3 install ${BUILDKITE_BUILD_PATH}/redefine-0.14.16.post5-py3-none-manylinux1_x86_64.whl
 redefine config set environment=staging
 redefine config set redefine_address=dune-tf-staging.redefine.dev
 redefine config set stable_branch=master
-# redefine config set dry_run=true
+redefine config set dry_run=true
 redefine start --verbose --cypress --worker
+npx cypress run
+
 
 echo "+++ Run Cypress tests"
 npm run test
