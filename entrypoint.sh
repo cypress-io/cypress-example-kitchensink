eb8d65da170afd21ebb9d534a4636654800b9597
liricooli <liricooli@gmail.com>
2023-06-01 20:24:02 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,31 +31,8 @@
 # npx cypress run
 env
 cat /app/redefine/1.txt
-python <<EOP
-import os
-
-def get_values(filename, index, n):
-    values = []
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            line = line.strip()
-            if line:  # Ignore empty lines
-                values.append(line)
-
-    result = []
-    for i, value in enumerate(values):
-        if (i + 1) % n == index+1 % n:
-            result.append(value)
-
-    return result
-
-filename = "/app/redefine/1.txt"
-index = int(os.environ["BUILDKITE_PARALLEL_JOB_COUNT"])
-n = int(os.environ["BUILDKITE_PARALLEL_JOB"])
-lst = get_values(filename,index,n)
-print(lst)
-EOP
+output=$(python /app/script.py)
 
 echo "+++ Run Cypress tests"
 # npm run test
+npm start & cypress run --spec ${output}
