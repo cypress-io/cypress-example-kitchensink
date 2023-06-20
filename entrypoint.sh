4286c87f6e9605f3c7d86b8a702fb2d0a6370cd4
liricooli <liricooli@gmail.com>
2023-06-20 23:22:46 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,32 @@
 ls /app
 ls /app/redefine
 env
+
+cat > /app/redefine/redefine_specs.txt<< EOF
+cypress/e2e/2-advanced-examples/network_requests.cy.js
+cypress/e2e/2-advanced-examples/actions.cy.js
+cypress/e2e/2-advanced-examples/files.cy.js
+cypress/e2e/2-advanced-examples/querying.cy.js
+cypress/e2e/2-advanced-examples/aliasing.cy.js
+cypress/e2e/2-advanced-examples/misc.cy.js
+cypress/e2e/2-advanced-examples/location.cy.js
+cypress/e2e/2-advanced-examples/assertions.cy.js
+cypress/e2e/2-advanced-examples/viewport.cy.js
+cypress/e2e/2-advanced-examples/waiting.cy.js
+cypress/e2e/1-getting-started/todo.cy.js
+cypress/e2e/2-advanced-examples/utilities.cy.js
+cypress/e2e/2-advanced-examples/window.cy.js
+cypress/e2e/2-advanced-examples/spies_stubs_clocks.cy.js
+cypress/e2e/2-advanced-examples/traversal.cy.js
+cypress/e2e/2-advanced-examples/storage.cy.js
+cypress/e2e/2-advanced-examples/connectors.cy.js
+cypress/e2e/2-advanced-examples/cookies.cy.js
+cypress/e2e/2-advanced-examples/cypress_api.cy.js
+cypress/e2e/2-advanced-examples/navigation.cy.js
+EOF
+
+
+
 echo "foo"
 npm install cypress@10.7
 pip3 uninstall -y redefine
