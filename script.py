95099d194be9b0a17f7b3000b02a118e1e2a17e4
liricooli <liricooli@gmail.com>
2023-06-20 23:42:24 +0300

    fix pyscript path

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ def get_values(filename, index, n):
 
     return result
 
-filename = "/app/redefine/specs.txt"
+filename = "/app/redefine/redefine_specs.txt"
 index = int(os.environ["BUILDKITE_PARALLEL_JOB"])
 n = int(os.environ["BUILDKITE_PARALLEL_JOB_COUNT"])
 lst = get_values(filename,index,n)
