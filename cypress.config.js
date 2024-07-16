71b216435909db2f357d887ffa986f697f31d7f6
David Kasper <dave.m.kasper@gmail.com>
2024-07-16 12:11:37 -0500

    commit the mess

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'projectId': '7okrxu',
+  'projectId': '6dty9k',
   e2e: {},
 }
--- cypress/e2e/2-advanced-examples/actions.cy.js
+++ cypress/e2e/2-advanced-examples/actions.cy.js
@@ -6,14 +6,29 @@ context('Actions', () => {
   })
 
   // eslint-disable-next-line
-  it.only('works', () => {
+  it('works', () => {
     cy.get('#outer').should('contain', 'Outer')
     // cy.get('#outer').should('contain', 'Inner')
   })
 
   // eslint-disable-next-line
-  it.only('does not work', () => {
+  it('does not work', () => {
     cy.get('#outer').should('contain', 'Outer2')
     // cy.get('#outer').should('contain', 'Inner2')
+  })
+
+  // eslint-disable-next-line
+  it('talks to an iframe', () => {
+    cy.get('#example-tier-1')
+      .its('0.contentDocument.body')
+      .should('contain', "I'm Tier 1 - h3")
+  })
+
+  it('talks to the waffles', () => {
+    cy.visit('http://localhost:8080/commands/donkey-content')
+
+    cy.get('#example-tier-1')
+      .its('0.contentDocument.body')
+      .should('contain', "I'm Tier 1 - h3")
   })
 })
